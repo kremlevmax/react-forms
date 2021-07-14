6acdff8aefd7c762d61dafb8c7d8b3b8b8ab2cc5
kremlevmax <kremlevmax1989@gmail.com>
2021-07-14 15:57:45 -0700

    Name Form Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,39 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [nameFormIsTouched, setNameFormIsTouched] = useState(false);
+
+  const isEnteredValueValid = enteredName.trim() !== "";
+  const isNameValid = !(!isEnteredValueValid && nameFormIsTouched);
+  let isFormValid = isEnteredValueValid;
+  console.log(isFormValid);
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameOnBlurHandler = (event) => {
+    setNameFormIsTouched(true);
+  };
+
+  const formCSSClass = isNameValid ? "form-control" : "form-control invalid";
+
   return (
     <form>
-      <div className='form-control'>
+      <div className={formCSSClass}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={nameChangeHandler}
+          onBlur={nameOnBlurHandler}
+          value={enteredName}
+        />
+        {!isNameValid && <p className='error-text'>Name shouldn't be empty</p>}
       </div>
-      <div className="form-actions">
-        <button>Submit</button>
+      <div className='form-actions'>
+        <button disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   );
